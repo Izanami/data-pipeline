87435124ce5bdb97c2b53205cf8a5f4ffd3623b8
Adrien Jeser <adrien@jeser.me>
2017-03-13 11:16:15 +0100

    Input pipeline (#14)

---
+++ src/input.h
@@ -0,0 +1,44 @@
+/*
+ *Data-Pipeline
+ *Copyright © 2017 Adrien Jeser <adrien@jeser.me>
+ *
+ *This program is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU Affero General Public License as
+ *published by the Free Software Foundation, either version 3 of the
+ *License, or (at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU Affero General Public License for more details.
+ *
+ *You should have received a copy of the GNU Affero General Public License
+ *along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef DP_INPUT_H_
+#define DP_INPUT_H_
+
+#include "pipeline.h"
+
+/*! \defgroup input_pipeline Input
+ * \brief File pipeline
+ *
+ *  \{
+ */
+
+/// \brief Factory of input pipeline.
+DpPipeline* DpInputNew(void) __attribute__((warn_unused_result));
+
+// \brief Constructor of input pipeline.
+void DpInputCreate(DpPipeline**) __attribute__((nonnull));
+
+// \brief Initialize of input pipeline.
+void DpInputInit(DpPipeline* self) __attribute__((nonnull));
+
+/*!
+ * \}
+ */
+
+#endif /* ifndef DP_INPUT_H_ */
